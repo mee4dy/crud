deaa192504783ed9da035e87a085d8ee5319dc51
mee4dy <mee4dy@gmail.com>
2024-08-01 21:54:29 +0300

    Fix merge objects

--- common/helpers/merge.helper.js
+++ common/helpers/merge.helper.js
@@ -2,8 +2,14 @@ import * as _ from 'lodash';
 
 export function merge(objA, objB) {
   return _.mergeWith(objA, objB, (a, b) => {
-    if (!_.isObject(b)) return b;
+    if (Array.isArray(a) && Array.isArray(b)) {
+      return [...a, ...b];
+    }
 
-    return Array.isArray(a) ? [...a, ...b] : { ...a, ...b };
+    if (_.isObject(a) && _.isObject(b)) {
+      return { ...a, ...b };
+    }
+
+    return b;
   });
 }

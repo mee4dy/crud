8cc4f28f4f01915441a81bad553b1ad72f249fe4
mee4dy <mee4dy@gmail.com>
2025-07-04 01:53:38 +0300

    Fix override default values

--- packages/crud-common/src/helpers/merge.helper.js
+++ packages/crud-common/src/helpers/merge.helper.js
@@ -2,11 +2,7 @@ import * as _ from 'lodash';
 
 export function merge(objA, objB) {
   return _.mergeWith(objA, objB, (a, b) => {
-    if (Array.isArray(a) && Array.isArray(b)) {
-      return [...a, ...b];
-    }
-
-    if (_.isObject(a) && _.isObject(b)) {
+    if (_.isPlainObject(a) && _.isPlainObject(b)) {
       return { ...a, ...b };
     }
 
